e7b6e4edcc04f4d9512f7842a26278d46314a91e
Ania <kujawanna@gmail.com>
2022-10-14 18:55:20 +0200

    update styles and react to 17

--- src/components/MusicianToolbar/MusicianToolbar.styles.ts
+++ src/components/MusicianToolbar/MusicianToolbar.styles.ts
@@ -1,5 +1,5 @@
 import { Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 type Props = {
   active: boolean;
