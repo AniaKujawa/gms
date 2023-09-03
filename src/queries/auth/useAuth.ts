b4ba18ea32611fefdcfaeeac511b13105a226287
Ania <kujawanna@gmail.com>
2023-09-03 21:22:41 +0200

    add use session type

--- src/queries/auth/useAuth.ts
+++ src/queries/auth/useAuth.ts
@@ -3,7 +3,6 @@ import { useSession } from "next-auth/react";
 export const useAuth = () => {
   const { data } = useSession();
   const headers: Record<string, string> = {};
-  const enabled = false;
 
   if (data?.token) {
     headers.Authorization = `Bearer ${data.token}`;
