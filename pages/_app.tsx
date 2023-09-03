7be4974a397106e44963052932b99f1aa4803d63
Ania <kujawanna@gmail.com>
2023-09-03 22:35:57 +0200

    add loader to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,12 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import nextI18NextConfig from '../next-i18next.config.js';
 import { SessionProvider } from 'next-auth/react';
 
-// import { ReactQueryDevtools } from 'react-query/devtools';
 import { theme } from '../src/styles/theme';
 import { UserContextProvider } from '../src/context/User';
 import { FeedbackContextProvider } from '../src/context/Feedback';
 import { FeedbackAlert } from '../src/shared/components/Feedback';
+import { usePageLoading } from '../src/hooks/usePageLoading';
+import { LoadingLayout } from '../src/layout/LoadingLayout';
 
 import '../src/styles/global.css';
 
@@ -25,24 +26,25 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient());
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => page);
+  const { isPageLoading } = usePageLoading();
 
   return (
-    // <CacheProvider value={clientSideEmotionCache}>
     <SessionProvider session={session}>
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <UserContextProvider>
             <FeedbackContextProvider>
-              {getLayout(<Component {...pageProps} />)}
+              {getLayout(
+                <LoadingLayout isLoading={isPageLoading}>
+                  <Component {...pageProps} />
+                </LoadingLayout>)}
               <FeedbackAlert />
             </FeedbackContextProvider>
           </UserContextProvider>
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </QueryClientProvider>
       </ThemeProvider >
     </SessionProvider>
-    // </CacheProvider>
   );
 }
 
